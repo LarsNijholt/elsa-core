ba4ea784b658cf764a89d187ee7f327d29a17463
Lars Nijholt <lni@quadira.com>
2025-01-07 13:57:56 +0100

    Add tests

---
+++ test/component/Elsa.Workflows.ComponentTests/Scenarios/WorkflowInstanceCreation/CreateWorkflowInstanceTests.cs
@@ -0,0 +1,60 @@
+﻿using Elsa.Common.Models;
+using Elsa.Workflows.ComponentTests.Abstractions;
+using Elsa.Workflows.ComponentTests.Fixtures;
+using Elsa.Workflows.ComponentTests.Scenarios.WorkflowInstanceCreation.Workflows;
+using Elsa.Workflows.Management;
+using Elsa.Workflows.Models;
+using Elsa.Workflows.Runtime;
+using Elsa.Workflows.Runtime.Messages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Workflows.ComponentTests.Scenarios.WorkflowInstanceCreation;
+
+public class CreateWorkflowInstanceTests : AppComponentTest
+{
+    private readonly IWorkflowRuntime _workflowRuntime;
+    private readonly IWorkflowInstanceManager _workflowInstanceManager;
+    
+    public CreateWorkflowInstanceTests(App app) : base(app)
+    {
+        _workflowRuntime = Scope.ServiceProvider.GetRequiredService<IWorkflowRuntime>();
+        _workflowInstanceManager = Scope.ServiceProvider.GetRequiredService<IWorkflowInstanceManager>();
+    }
+    
+    [Fact]
+    public async Task CreateWorkflowInstance_WithSetNameConfigured_ShouldOverrideDefinitionName()
+    {
+        var workflowClient = await _workflowRuntime.CreateClientAsync();
+        await workflowClient.CreateAndRunInstanceAsync(new CreateAndRunWorkflowInstanceRequest
+        {
+            WorkflowDefinitionHandle = WorkflowDefinitionHandle.ByDefinitionId(SetNameWorkflow.DefinitionId, VersionOptions.Published)
+        });
+        
+        var result = await workflowClient.RunInstanceAsync(RunWorkflowInstanceRequest.Empty);
+
+        var instance = await _workflowInstanceManager.FindByIdAsync(result.WorkflowInstanceId);
+       
+        Assert.Equal(WorkflowStatus.Finished, result.Status);
+        Assert.Equal(WorkflowSubStatus.Finished, result.SubStatus);
+        Assert.Equal("Completed-Test", instance?.Name);
+    }
+    
+    
+    [Fact]
+    public async Task CreateWorkflowInstance_WithNoSetNameConfigured_ShouldHaveDefinitionName()
+    {
+        var workflowClient = await _workflowRuntime.CreateClientAsync();
+        await workflowClient.CreateAndRunInstanceAsync(new CreateAndRunWorkflowInstanceRequest
+        {
+            WorkflowDefinitionHandle = WorkflowDefinitionHandle.ByDefinitionId(UseWorkflowNameWorkflow.DefinitionId, VersionOptions.Published)
+        });
+        
+        var result = await workflowClient.RunInstanceAsync(RunWorkflowInstanceRequest.Empty);
+
+        var instance = await _workflowInstanceManager.FindByIdAsync(result.WorkflowInstanceId);
+       
+        Assert.Equal(WorkflowStatus.Finished, result.Status);
+        Assert.Equal(WorkflowSubStatus.Finished, result.SubStatus);
+        Assert.Equal("Test-Workflow", instance?.Name);
+    }
+}
---
+++ test/component/Elsa.Workflows.ComponentTests/Scenarios/WorkflowInstanceCreation/Workflows/SetNameWorkflow.cs
@@ -0,0 +1,25 @@
+﻿using Elsa.Workflows.Activities;
+using Elsa.Workflows.Models;
+
+namespace Elsa.Workflows.ComponentTests.Scenarios.WorkflowInstanceCreation.Workflows;
+
+public class SetNameWorkflow : WorkflowBase
+{
+    public static readonly string DefinitionId = Guid.NewGuid().ToString();
+
+    protected override void Build(IWorkflowBuilder builder)
+    {
+        builder.WithDefinitionId(DefinitionId);
+        builder.Name = "Test-Workflow";
+        builder.Root = new Sequence
+        {
+            Activities =
+            {
+                new SetName
+                {
+                    Value = new Input<string>("Completed-Test")
+                }
+            }
+        };
+    }
+}
